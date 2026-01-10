0de85eb88347d3ddf87aa46a2b33d4166210f5ab
Sukhrobzhon Talibzhanov <tolibjanov05@gmail.com>
2026-01-11 01:11:27 +0500

    updated repository

---
+++ Infrastructure/MovieApi.Persistance/Context/MovieContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistance.Context
+{
+    public class MovieContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-1GEE0JQ;Initial Catalog=ApiMovieDb;User Id=sa;Password=1;TrustServerCertificate=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+    }
+}
