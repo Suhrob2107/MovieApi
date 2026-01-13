bb4ece35d2879cc177146ae86f85babc134762a8
Sukhrobzhon Talibzhanov <tolibjanov05@gmail.com>
2026-01-13 23:56:39 +0500

    updated repository

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Commands/MovieCommands/UpdateMovieCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands
+{
+    public class UpdateMovieCommand
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string CoverImageUrl { get; set; } = string.Empty;
+        public string Rating { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedYear { get; set; } = string.Empty;
+        public bool Status { get; set; }
+    }
+}
