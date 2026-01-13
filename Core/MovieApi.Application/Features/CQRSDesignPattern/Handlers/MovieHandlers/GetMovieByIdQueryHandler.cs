3a95f7c058deb763872650fe53ac1da57b9e59e7
Sukhrobzhon Talibzhanov <tolibjanov05@gmail.com>
2026-01-14 01:40:36 +0500

    updated repository

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMovieByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Queries.MovieQueries;
+using MovieApi.Application.Features.CQRSDesignPattern.Results.MovieResults;
+using MovieApi.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class GetMovieByIdQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetMovieByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async Task<GetMovieByIdQueryResult> Handle(GetMovieByIdQuery query)
+        {
+            var values = await _context.Movies.FindAsync(query.MovieId);
+            return new GetMovieByIdQueryResult
+            {
+                MovieId = values.MovieId,
+                Title = values.Title,
+                CoverImageUrl = values.CoverImageUrl,
+                Rating = values.Rating,
+                Description = values.Description,
+                Duration = values.Duration,
+                ReleaseDate = values.ReleaseDate,
+                CreatedYear = values.CreatedYear,
+                Status = values.Status
+            };
+        }
+    }
+}
---
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.CategoryCommands;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
+
+        public CategoriesController(GetCategoryQueryHandler getCategoryQueryHandler, GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, CreateCategoryCommandHandler createCategoryCommandHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler)
+        {
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            _getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+            _updateCategoryCommandHandler = updateCategoryCommandHandler;
+            _removeCategoryCommandHandler = removeCategoryCommandHandler;
+        }
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var result = await _getCategoryQueryHandler.Handle();
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+        {
+            await _createCategoryCommandHandler.Handle(command);
+            return Ok("Kategori Eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryCommand command)
+        {
+            await _updateCategoryCommandHandler.Handle(command);
+            return Ok("Kategori Güncellendi");
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(RemoveCategoryCommand command)
+        {
+            await _removeCategoryCommandHandler.Handle(command);
+            return Ok("Kategori Silindi");
+        }
+    }
+}
