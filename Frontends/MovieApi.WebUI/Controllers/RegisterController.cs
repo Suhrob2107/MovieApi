0d54efecb62e97b970cff666447c8f3dc1977f43
Sukhrobzhon Talibzhanov <tolibjanov05@gmail.com>
2026-01-24 20:44:08 +0500

    updated repository

---
+++ Frontends/MovieApi.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.UserRegisterDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(CreateUserRegisterDto createUserRegisterDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createUserRegisterDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7048/api/Register", stringContent);
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+            ViewBag.Error = "Bir hata olustu, lutfen tekrar deneyiniz.";
+            return View();
+        }
+    }
+}
